6dda539842ab1916c7d01654c2da340ae62c6d85
badogan <basri.dogan.71@gmail.com>
2019-12-16 17:37:28 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist1 = Artist.create(name:"Basri Artist")
+
+song1 = Song.create(title:"It is my life")
+song1.artist = artist1
+song1.save
+
+song2 = Song.create(title:"Another life - It is his life")
+song2.artist = artist1
+song2.save
