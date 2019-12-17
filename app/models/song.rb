0acefdf91f9e4a28da8c524936f8f06585aa897c
github username <github email address>
2019-12-17 15:47:16 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def print_name_title
+    "#{self.artist.name} - #{self.title}"
+  end
+
+  def artist_name
+    self.artist.name 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artist1 = Artist.create(name: "Barbara Wicks")
+artist2 = Artist.create(name: "Samuel Harding")
+artist3 = Artist.create(name: "Beyonce")
+
+song1 = Song.create(title: "singing in the rain", artist_id: artist1.id)
+song2 = Song.create(title: "my girl wants to party", artist_id: artist2.id)
+song3 = Song.create(title: "mellow yellow", artist_id: artist1.id)
+song4 = Song.create(title: "sinlge ladies", artist_id: artist3.id)
